0baec789c4a963d4b8f7fc2ebe4839505a7d51d5
prykhozhai-qualium <prykhozhai@qualium-systems.com>
2022-08-05 20:52:38 +0300

    Metalitix

--- metalitix-tool/app.js
+++ metalitix-tool/app.js
@@ -31691,7 +31691,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-const LOGGER_API = 'https://metalitix-dev.aircards.io/api/v1/xr-analytics';
+const LOGGER_API = 'https://app.metalitix.com/api/v1/xr-analytics';
 // const LOGGER_API = 'https://metalitix-staging.aircards.io/api/v1/xr-analytics';
 
 const AnalyticsLogger = {
